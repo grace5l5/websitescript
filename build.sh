76f1ee9d9fe178a33ba5a5dd512d91c04d2f7df0
Grace5l5 <gs.leungshing@gmail.com>
2018-09-19 13:30:24 -0700

    created bash.sh and generated pages

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/blogs.html templates/bottom.html > docs/blogs.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
