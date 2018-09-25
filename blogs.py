b5666be83f33391a4d8a69ce97c381a944915ec3
Grace5l5 <gs.leungshing@gmail.com>
2018-09-25 15:38:15 -0700

    Generate blog pages - issues with side links

--- blogs.py
+++ blogs.py
@@ -30,24 +30,27 @@ blog_posts = [
     "filename": "blog/1.html",
     "date": "September 3rd, 2018",
     "title": "My experience so far as a noob at Kickstart Coding",
+	"output": "docs/blog1.html"
 	},
 	{
 	"filename": "blog/2.html",
 	"date": "September 10th, 2018",
 	"title": "So far I really like Ubuntu Linux",
+	"output": "docs/blog2.html"
 	}, 
 	{
 	"filename": "blog/3.html",
 	"date": "September 15th, 2018",
 	"title": "My thoughts on Python so far",
+	"output": "docs/blog3.html"
 	}
 ]
 
 
 def main():
 # Iterate through the pages list and writes into the output file the title and the content
-	blog_page = blog_main()
-	open('content/blogs1.html','w+').write(blog_page)
+	blog_main()
+	blog_pages()
 	for page in pages:
 		content = open(page['filename'])
 		title = page['title']
@@ -67,33 +70,25 @@ def apply_title(title, content):
 def blog_main():
 # Replace the date of the blogs
 	blog = open('content/blogs.html').read()
-	main_blog1 = blog.replace('{{date1}}', blog_posts[0]['date'])
-	main_blog2 = main_blog1.replace('{{date2}}', blog_posts[1]['date'])
-	main_blog = main_blog2.replace('{{date3}}', blog_posts[2]['date'])
-	return main_blog
-'''
+	date1 = blog.replace('{{date1}}', blog_posts[0]['date'])
+	date2 = date1.replace('{{date2}}', blog_posts[1]['date'])
+	date3 = date2.replace('{{date3}}', blog_posts[2]['date'])
+	main_blog = date3.replace('{{date}}', blog_posts[len(blog_posts)-1]['title'])
+	open('content/blogs1.html','w+').write(main_blog)
 
-def blog_page():
-	for count in range(len(blog_posts)):
-		blog_template = open('blog_template.html').read()
-		blog_content = open(blog_posts[count]['filename'])
-		blog_title = blog_posts[count]['title']
-		published = blog_posts[count]['date']
+
+def blog_pages():
+# Create blog page for each blog
+	for blog in blog_posts:
+		blog_template = open('blog/blog_template.html').read()
+		blog_content = open(blog['filename']).read()
+		blog_title = blog['title']
+		published = blog['date']
 		content_blog = blog_template.replace('{{content}}', blog_content)
-		title_blog = content_blog.replace('{{title}}', title)
-		full_blog = title_blog.replace('{{date}}', date)
-		return full_blog
-'''
+		title_blog = content_blog.replace('{{title}}', blog_title)
+		full_blog = title_blog.replace('{{date}}', published)
+		open(blog['output'],'w+').write(full_blog)
 
-'''
-bp_html = '<ol>'
-for blog_post in blog_posts:
-	bp_html = bp_html + '<li><a href="' + blog_post['title'] + '</a></li>'
-bp_html
-
-finished_index_page.replace('{{recent_blog_posts}}', bp_html)
-
-'''
 
 if __name__ == "__main__":
     main()
