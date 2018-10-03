537adc53438cf0280867406ae397c746c0106138
Grace5l5 <gs.leungshing@gmail.com>
2018-10-04 06:10:32 +0800

    Manage.py with CLI arguments

--- manage.py
+++ manage.py
@@ -1,6 +1,23 @@
 
 import utils
-utils.main()
+import sys
 
-utils.content()
+# Check for the number of arguments and if command is manage.py
+if len(sys.argv) > 1 and sys.argv[0] == 'manage.py':
+	command = sys.argv[1]
+	print(command)
+	if command == "build":
+		utils.main()
+		utils.content()
+	elif command == "new":
+		print("New page was specified")
+	else:
+		print("Please specify ’build’ or ’new’")
 
+# User input is has only one argument or argument is not equal to 'manage.py'
+else:
+	print('''
+	Usage:
+	Rebuild site:		python manage.py build
+	Create new page:	python manage.py new
+		''')
